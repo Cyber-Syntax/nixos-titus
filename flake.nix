b0e81882a584c75d0b46b659686aa34de7c45623
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2024-02-21 14:24:52 +0300

    update folder names

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       system = "x86_64-linux";
       specialArgs = {inherit inputs self user;};
       modules = [
-        ./system/configuration.nix
+        ./nixos/configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
